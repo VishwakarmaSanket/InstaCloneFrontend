e23e9b874f88281234206388c07909d6e37af613
Sanket Vishwakarma <your-email@example.com>
2026-02-27 20:53:50 +0530

    feat(auth): refactor registration to use context-based handler and improve loading state

--- src/features/auth/pages/Register.jsx
+++ src/features/auth/pages/Register.jsx
@@ -3,37 +3,57 @@ import "../style/form.scss";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router";
 
 const Register = () => {
+  const { loading, registerHandler } = useAuth();
+
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+
   async function submitHandler(e) {
     e.preventDefault();
 
-    await axios
-      .post(
-        "http://localhost:3000/api/auth/register",
-        {
-          username,
-          email,
-          password,
-        },
-        {
-          withCredentials: true,
-        },
-      )
-      .then((res) => {
-        console.log(res);
-      });
+    await registerHandler(username, email, password);
+    console.log("User Registered !!!");
+    navigate("/");
   }
+
+  if (loading)
+    return (
+      <main>
+        <div className="svg-loader">
+          <svg viewBox="0 0 100 100">
+            <circle
+              className="track"
+              cx="50"
+              cy="50"
+              r="40"
+              fill="none"
+              strokeWidth="6"
+            />
+            <circle
+              className="progress"
+              cx="50"
+              cy="50"
+              r="40"
+              fill="none"
+              strokeWidth="6"
+            />
+          </svg>
+        </div>
+      </main>
+    );
 
   return (
     <main>
       <div className="form-container">
         <h1>Register</h1>
-        <form>
+        <form onSubmit={submitHandler}>
           <input
             onInput={(e) => {
               setUsername(e.target.value);
